160e6b4488522b9ebe9ad706f9918e9b4ca6d74a
yanglklk <42258552+yanglklk@users.noreply.github.com>
2019-01-04 15:39:44 +0800

    commment data path

--- deepst/config.py
+++ deepst/config.py
@@ -10,11 +10,13 @@ class Config(object):
         super(Config, self).__init__()
 
         DATAPATH = os.environ.get('DATAPATH')
+        '''The default DATAPATH variable is DATAPATH=[path_to_DeepST]/data'''
         if DATAPATH is None:
             if platform.system() == "Windows" or platform.system() == "Linux":
                 # DATAPATH = "D:/data/traffic_flow"
             # elif platform.system() == "Linux":
                 DATAPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
+                '''从当前文件夹转到data目录下'''
             else:
                 print("Unsupported/Unknown OS: ", platform.system, "please set DATAPATH")
         self.DATAPATH = DATAPATH
